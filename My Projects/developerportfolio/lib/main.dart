658e9c62e69aae0ae188449c2abd53219ef5f93f
Charles Chua <chuacharlesbon@gmail.com>
2023-09-03 21:31:55 +0800

    Updated: Main App - New Route

--- My Projects/developerportfolio/lib/main.dart
+++ My Projects/developerportfolio/lib/main.dart
@@ -19,6 +19,16 @@ void main() {
               child: MyHomeScreen()
             ),
           ),
+        ),
+        GoRoute(
+          path: '/works',
+          name: 'works',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: AppTitleBuilder(
+              title: "Developer's Portfolio | Works",
+              child: MyHomeScreen()
+            ),
+          ),
         )
       ],
     ),
@@ -34,7 +44,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Developer's Portfolio",
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -47,24 +57,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
